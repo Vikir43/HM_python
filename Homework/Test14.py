12c5ff64246d698a99c8f77ab1bc0091de3372d0
Viki <viki333@list.ru>
2023-04-05 21:22:39 +0300

    Change fail Homework3

--- Homework/Test14.py
+++ Homework/Test14.py
@@ -1,5 +1,6 @@
 ## Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
 ## num = int(input('Введите число: '))
+
 n = int(input('Введите число ограничение: '))
 match = 1
 while match < n:    
@@ -8,3 +9,9 @@ while match < n:
         else:
             match = match * 2
             print(match, end= ' ')
+
+n = int (input())
+i = 0
+while 2 ** i<= n:
+     print(2 ** i)
+     i +=1
